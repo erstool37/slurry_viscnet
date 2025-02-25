f2b2d40a7500ec8894f3185b8c4e502cd0ae85d7
erstool37 <jongwonsohn@gmail.com>
2025-02-25 17:11:27 +0900

    config split before JAPAN

---
+++ setup.py
@@ -0,0 +1,9 @@
+# CUDA test
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("CUDA version in PyTorch:", torch.version.cuda) 
+print("CUDA available:", torch.cuda.is_available()) 
+print("Number of GPUs:", torch.cuda.device_count())  
+print("GPU Name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU detected")
+
