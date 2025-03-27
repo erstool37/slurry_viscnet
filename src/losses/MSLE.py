92fe5108cf5abe65dc4ef2beada73853bbed4ebd
root <jongwonsohn@gmail.com>
2025-03-27 11:08:15 +0000

    push

--- src/losses/MSLE.py
+++ src/losses/MSLE.py
@@ -16,6 +16,7 @@ class MSLELoss(nn.Module):
         # predicted kinVisc calculation in unnormalized version
         pred_den = logdescaler(pred[:,0], "density").unsqueeze(-1).to(pred.device)
         pred_dynVisc = logdescaler(pred[:,1], "dynamic_viscosity").unsqueeze(-1).to(pred.device)
+        
         pred_kinVisc = (pred_dynVisc / pred_den)
 
         pred = torch.cat((pred, pred_kinVisc), dim=-1)
@@ -25,6 +26,7 @@ class MSLELoss(nn.Module):
         target[:,3] = ans_kinVisc
 
         loss = (torch.log1p(pred) - torch.log1p(target)) ** 2 
+        # loss = (pred-target) ** 2
 
         loss_den = loss[:, 0].mean()
         loss_dynvisc = loss[:, 1].mean()
@@ -32,17 +34,18 @@ class MSLELoss(nn.Module):
         loss_kinvisc = loss[:, 3].mean()
         # loss_kinvisc = F.mse_loss(pred[:,3], target[:,3]) # already mean
 
-        loss_total = 1 * loss_den +  1 * loss_surfT + 1 * loss_kinvisc + 1* loss_dynvisc
+        loss_total = 5 * loss_den +  1 * loss_surfT + 5 * loss_dynvisc
         # loss_total = 1 * loss_den +  1 * loss_surfT + 10**4 * loss_kinvisc
 
         wandb.log({"loss_den": loss_den})
         wandb.log({"loss_visc": loss_dynvisc})
         wandb.log({"loss_surf": loss_surfT})
-        wandb.log({"loss_kinvisc": loss_kinvisc})
+        # wandb.log({"loss_kinvisc": loss_kinvisc})
 
-        # wandb.log({"loss_visc_chunk": 1 * loss_dynvisc})
-        # wandb.log({"loss_surf_chunk": 1 * loss_surfT})
-        # wandb.log({"loss_kinvisc_chunk": 1 * loss_kinvisc})
+        wandb.log({"loss_den_chunk": 5 * loss_dynvisc})
+        wandb.log({"loss_visc_chunk": 5 * loss_dynvisc})
+        wandb.log({"loss_surf_chunk": 1 * loss_surfT})
+        # wandb.log({"loss_kinvisc_chunk": 10**3 * loss_kinvisc})
         
         return loss_total
 
