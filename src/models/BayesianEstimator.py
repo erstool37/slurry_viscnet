498f0f328fd261c05560c64bc17d23fc3b0cc245
erstool37 <jongwonsohn@gmail.com>
2025-04-01 16:40:16 +0900

    loss update

--- src/models/BayesianEstimator.py
+++ src/models/BayesianEstimator.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
 import torch
 from torchvision import models
+import torch.nn.functional as F
 
-class ViscosityEstimator(nn.Module):
+class BayesianEstimator(nn.Module):
     def __init__(self, lstm_hidden_size, lstm_layers, output_size, dropout, cnn, cnn_train):
-        super(ViscosityEstimator, self).__init__()
+        super(BayesianEstimator, self).__init__()
         self.resnet = getattr(models, cnn)(pretrained=True)
         self.cnn = nn.Sequential(*list(self.resnet.children())[:-1])
         self.cnn_out_features = 512
@@ -13,19 +14,21 @@ class ViscosityEstimator(nn.Module):
             param.requires_grad = cnn_train
 
         self.lstm = nn.LSTM(input_size=self.cnn_out_features, hidden_size=lstm_hidden_size, num_layers=lstm_layers, batch_first=True, dropout=dropout)
-        self.fc = nn.Linear(lstm_hidden_size, output_size)
+        
+        self.fc_mu = nn.Linear(lstm_hidden_size, output_size)
+        self.fc_sigma = nn.Linear(lstm_hidden_size, output_size)
     
     def forward(self, x):
-        """ x: (batch_size, sequence_length, channels, height, width)"""
         batch_size, frames, C, H, W = x.size()
         x = x.view(batch_size * frames, C, H, W)
 
-        video_features = self.cnn(x) 
-        video_features = video_features.view(batch_size, frames, -1) 
+        video_features = self.cnn(x)  # (B*T, 512, 1, 1)
+        video_features = video_features.view(batch_size, frames, -1)  # (B, T, 512)
 
         lstm_out, _ = self.lstm(video_features)
-        lstm_last_out = lstm_out[:, -1, :]
-        
-        viscosity = self.fc(lstm_last_out)
-        
-        return viscosity
+        lstm_last_out = lstm_out[:, -1, :]  # (B, H)
+
+        mu = self.fc_mu(lstm_last_out)                      # (B, output_size)
+        sigma = F.softplus(self.fc_sigma(lstm_last_out)) + 1e-6
+
+        return mu, sigma
--- src/models/ResnetLSTM.py
+++ src/models/ResnetLSTM.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
 import torch
 from torchvision import models
-import torch.nn.functional as F
 
-class BayesianViscosityEstimator(nn.Module):
+class ResnetLSTM(nn.Module):
     def __init__(self, lstm_hidden_size, lstm_layers, output_size, dropout, cnn, cnn_train):
-        super(BayesianViscosityEstimator, self).__init__()
+        super(ResnetLSTM, self).__init__()
         self.resnet = getattr(models, cnn)(pretrained=True)
         self.cnn = nn.Sequential(*list(self.resnet.children())[:-1])
         self.cnn_out_features = 512
@@ -14,21 +13,19 @@ class BayesianViscosityEstimator(nn.Module):
             param.requires_grad = cnn_train
 
         self.lstm = nn.LSTM(input_size=self.cnn_out_features, hidden_size=lstm_hidden_size, num_layers=lstm_layers, batch_first=True, dropout=dropout)
-        
-        self.fc_mu = nn.Linear(lstm_hidden_size, output_size)
-        self.fc_sigma = nn.Linear(lstm_hidden_size, output_size)
+        self.fc = nn.Linear(lstm_hidden_size, output_size)
     
     def forward(self, x):
+        """ x: (batch_size, sequence_length, channels, height, width)"""
         batch_size, frames, C, H, W = x.size()
         x = x.view(batch_size * frames, C, H, W)
 
-        video_features = self.cnn(x)  # (B*T, 512, 1, 1)
-        video_features = video_features.view(batch_size, frames, -1)  # (B, T, 512)
+        video_features = self.cnn(x) 
+        video_features = video_features.view(batch_size, frames, -1) 
 
         lstm_out, _ = self.lstm(video_features)
-        lstm_last_out = lstm_out[:, -1, :]  # (B, H)
-
-        mu = self.fc_mu(lstm_last_out)                      # (B, output_size)
-        sigma = F.softplus(self.fc_sigma(lstm_last_out)) + 1e-6
-
-        return mu, sigma
+        lstm_last_out = lstm_out[:, -1, :]
+        
+        viscosity = self.fc(lstm_last_out)
+        
+        return viscosity
