9c3a46b00e017f55cb4fd23b711e30f86c92a8e5
root <jongwonsohn@gmail.com>
2025-04-02 11:44:26 +0000

    success is sweet

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,4 +1,16 @@
-python3 src/utils/preprocess.py -c configs/config.yaml
-python3 src/main.py -c configs/config.yaml
+# python3 src/utils/preprocess.py -c configs/config.yaml
+# python3 src/main.py -c configs/config.yaml
 # python3 src/utils/preprocess.py -c configs/config2.yaml
 # python3 src/main.py -c configs/config2.yaml
+
+python3 src/utils/preprocess.py -c configs/config2.yaml
+python3 src/main.py -c configs/config2.yaml
+
+python3 src/utils/preprocess.py -c configs/config3.yaml
+python3 src/main.py -c configs/config3.yaml
+
+python3 src/utils/preprocess.py -c configs/config4.yaml
+python3 src/main.py -c configs/config4.yaml
+
+python3 src/utils/preprocess.py -c configs/config5.yaml
+python3 src/main.py -c configs/config5.yaml
