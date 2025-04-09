b7af84a54f78d268fc53325cec88254e831c8f26
root <jongwonsohn@gmail.com>
2025-04-09 05:42:33 +0000

    repo change

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
+cd slurry_viscnet
+git pull origin main
+pip install -r requirements.txt
 apt update
 apt install -y tmux
-cd slurry_viscnet
-pip install -r requirements.txt
-git pull origin main
 # vessl storage copy-file volume://vessl-storage/decay-visconly . 
 tmux new-session -d -s slave1
 # tmux attach -t slave1
